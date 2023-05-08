a2a127a7c4c20675258c5f63faa6074a8de8ba61
Chin Liang <chinholiang@gmail.com>
2023-05-07 21:05:24 -0500

    first commit

---
+++ data/misc/django_init.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS soccer_team_db;
+CREATE DATABASE soccer_team_db;
+
+\c soccer_team_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
